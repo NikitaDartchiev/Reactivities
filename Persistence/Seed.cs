3a5470d68df3fadc350b35e36cae103132a810f2
Nikita Dartchiev <ndartchiev@singular.co.za>
2023-01-23 13:05:53 +0200

    ens of section 12

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,14 +1,30 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context,
+            UserManager<AppUser> userManager)
         {
-            if (context.Activities.Any()) return;
-            
-            var activities = new List<Activity>
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Bob",UserName = "bob",Email = "bob@test.com"},
+                    new AppUser{DisplayName = "Jane",UserName = "jane", Email = "jane@test.com"},
+                    new AppUser{DisplayName = "Tom",UserName = "tom",Email = "tom@test.com"},
+                };
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+            if (!context.Activities.Any())
+            {
+
+                var activities = new List<Activity>
             {
                 new Activity
                 {
@@ -95,15 +111,16 @@ namespace Persistence
                 {
                     Title = "Future Activity 8",
                     Date = DateTime.Now.AddMonths(8),
-                    Description = "Activity 8 months in future", 
+                    Description = "Activity 8 months in future",
                     Category = "film",
                     City = "London",
                     Venue = "Cinema",
                 }
             };
 
-            await context.Activities.AddRangeAsync(activities);
-            await context.SaveChangesAsync();
+                await context.Activities.AddRangeAsync(activities);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
